dc94ddce2caa3a5a5595e92178e9ea27d3e2cf4a
nkbuddy <49331641+nkbuddy@users.noreply.github.com>
2023-04-19 16:57:15 -0400

    Updates

---
+++ precache-manifest.2bede3797087588e764fc2ed904e8d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce9a2880ca3ec1c99e94",
+    "url": "/adroit/static/js/main.ce9a2880.chunk.js"
+  },
+  {
+    "revision": "c78b49f22eda29250df4",
+    "url": "/adroit/static/css/1.10b8ab0d.chunk.css"
+  },
+  {
+    "revision": "c78b49f22eda29250df4",
+    "url": "/adroit/static/js/1.c78b49f2.chunk.js"
+  },
+  {
+    "revision": "a3729c6542c78f00313f",
+    "url": "/adroit/static/js/runtime~main.a3729c65.js"
+  },
+  {
+    "revision": "e669c0d3ccb5d7eabd929783cff2bf5a",
+    "url": "/adroit/index.html"
+  }
+];
